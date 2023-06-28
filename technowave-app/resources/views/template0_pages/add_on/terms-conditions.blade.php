da39b62dcb087f04c215b3f9c400816314509bee
Keshav Persad <kipersad1@hotmail.com>
2023-06-28 12:43:47 -0400

    Creating Addition Pages

---
+++ technowave-app/resources/views/template0_pages/add_on/terms-conditions.blade.php
@@ -0,0 +1,38 @@
+<x-layouts.layout-template0-home>
+
+<title>Techno Wave Ltd. || {{''}}Terms & Conditions</title>	
+
+<main id="main" class="main-site">
+
+<div class="container">
+
+    <div class="wrap-breadcrumb">
+        <ul>
+        <li class="item-link"><a href="{{ route ('home')}}" class="link">Home</a></li>
+            <li class="item-link"><span>Terms and Conditions</span></li>
+        </ul>
+    </div>
+</div>
+
+<div class="container pb-60">
+    <div class="row">
+        <div class="col-md-12">
+            <h3>Terms and Conditions</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto.</p>
+        </div>
+    </div>
+</div><!--end container-->
+
+</main>
+<!--main area-->
+
+</x-layouts.layout-template0-home>
